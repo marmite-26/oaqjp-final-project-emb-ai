d654d1b2511d5c02b3f055bf3b24f3b4d20df42b
Your Github User Name <email@example.com>
2026-02-14 04:19:05 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EmotionDetection',
+    version='1.0.0',
+    description='A library for emotion detection using Watson NLP',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+)
